48d4f8d16250fd45f8a4c228b75bef7b3d816edc
Amanda Cardoso <amandabocardoso0@gmail.com>
2024-11-30 17:15:38 -0300

    leftmenu

--- scripts/Script.js
+++ scripts/Script.js
@@ -1,3 +1,50 @@
 function openCloseMenu(){
-    var 
-}
+    let open = document.getElementById('open');
+    let close = document.getElementById('close');
+  
+    open.classList.toggle('hidden');
+    close.classList.toggle('hidden');
+  
+   if (open.classList.contains('hidden')) {
+      increaseWidth();
+    } else {
+      decreaseWith();
+    }
+  }
+  
+  function decreaseWith(){
+      let menu = document.getElementById('Nav-Bar-Esquerda');
+      let content = document.getElementById('contents');
+      let perfil = document.getElementById('perfil');
+  
+      content.classList.toggle('hidden');
+      menu.style.width = '0px';
+      perfil.classList.toggle('hidden');
+      adjustBtnMenuClosed()
+  
+  }
+  
+  function increaseWidth(){
+      let menu = document.getElementById('Nav-Bar-Esquerda');
+      let content = document.getElementById('contents');
+      let perfil = document.getElementById('perfil');
+  
+      perfil.classList.toggle('hidden');
+      menu.style.width = '270px';
+      content.classList.toggle('hidden');
+      adjustBtnMenuOpened()
+  }
+  
+  function adjustBtnMenuClosed(){
+      const element = document.querySelector("#Open-Close");
+      Object.assign(element.style, {
+          marginLeft: "0px",       
+      });
+  }
+  
+  function adjustBtnMenuOpened(){
+      const element = document.querySelector("#Open-Close");
+      Object.assign(element.style, {
+          marginLeft: "270px",       
+      });
+  }
